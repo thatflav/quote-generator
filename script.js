e55c6225ee1efddbaeb01325ccf1b8edcd5f5ef4
Flavius Bohaciuc <flaviusbohaciuc@gmail.com>
2020-08-14 22:43:30 +0100

    Fetch data with js, solve CORS issues with Heroku Server building my own proxy server and manipulate DOM with js

---
+++ script.js
@@ -0,0 +1,33 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuote = document.getElementById('new-quote');
+
+// Get Quote From API
+async function getQuote() {
+    const proxyUrl = 'https://secure-ridge-32711.herokuapp.com/'
+    const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        // If Author is blank, add 'Unknown'
+        if (data.quoteAuthor === '') {
+            authorText.innerText = 'Unknown'
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        // Reduce font size for long quotes
+        if (data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText;
+    } catch (error) {
+        getQuote();
+    }
+}
+
+// On Load
+getQuote();
