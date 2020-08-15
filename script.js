3cb4ab9fc4c5be1a8556429779726db02f38996d
Flavius Bohaciuc <flaviusbohaciuc@gmail.com>
2020-08-15 10:35:35 +0100

    Add twitter and new quote functionality.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
-const newQuote = document.getElementById('new-quote');
+const newQuoteBtn = document.getElementById('new-quote');
 
 // Get Quote From API
 async function getQuote() {
@@ -28,6 +28,18 @@ async function getQuote() {
         getQuote();
     }
 }
+
+//Tweet Quote
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // On Load
 getQuote();
